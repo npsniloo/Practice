fae599653d7bf232e037514c415b0094d8e744d7
n.pahlevan <niloofar.pahlevan@gmail.com>
2024-06-14 23:12:47 +0330

    change .net version

---
+++ BlazorAspCore/BlazorAspCore.Server/Pages/Shopping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorAspCore.Server.Pages
+{
+    public class ShoppingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
